d99e88476cf804c9614e537305512441bb708fef
Sarah Pierce <spierce@austin.rr.com>
2024-04-12 10:49:59 -0500

    change color

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	myFigure := figure.NewColorFigure("munchicken is Awesome!!!", "larry3d", "yellow", true)
+	myFigure := figure.NewColorFigure("munchicken is Awesome!!!", "larry3d", "green", true)
 	myFigure.Print()
 
 	if secret := os.Getenv("SECRET"); secret != "" {
